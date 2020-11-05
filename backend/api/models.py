eb3b8b4f7e62dd763117ef253095fa3025ea9457
Jaimyn <github@jaimyn.com.au>
2020-11-05 23:59:44 +1000

    removed python backend

--- backend/api/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class User(AbstractUser):
-    bio = models.TextField(max_length=500, blank=True)
--- backend/api/urls.py
+++
@@ -1,9 +0,0 @@
-# jDischarger API URL Configuration
-
-from django.contrib import admin
-from django.urls import include
-from django.urls import path
-
-urlpatterns = [
-    # path('admin/', admin.site.urls),
-]
